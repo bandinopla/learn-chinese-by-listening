81a1804cc2ded07d65690e098fb00f49d3a35c55
bandinopla <pablobandinopla@gmail.com>
2024-11-29 12:21:18 +0000

    added sound play/stop button in filter lines list

--- src/components/AudiosFilter.tsx
+++ src/components/AudiosFilter.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import { data } from "../data/data";
 import { CheckBox, CheckBoxState } from "./CheckBox";
+import { InlinePlayStopLine } from "./InlinePlayStopLine";
 
 export const AudiosFilter: FC <{ audioLinesState:CheckBoxState, onFilterChange?:(audioLinesState:number[])=>void }> = ({ audioLinesState }) => { 
 
     return data.lines.map( (line, i)=><div key={i} style={{ display:"flex", alignItems:"center", gap:6, marginBottom:3}}>
-        <CheckBox index={i} state={audioLinesState} shiftForSingleSelection/> {line.ch}
+        <CheckBox index={i} state={audioLinesState} shiftForSingleSelection/>  {line.ch} <InlinePlayStopLine line={line}/>
     </div> )
 }
---
+++ src/components/InlinePlayStopLine.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+import { CharacterInfo, Line } from "../data/types";
+import { ChineseAudio } from "./ChineseAudio";
+
+export const InlinePlayStopLine: FC<{ line:Line }> = ({ line }) => {
+    return <div style={{ fontSize:"0.5em"}}><ChineseAudio line={line} num={0}/></div>
+}
